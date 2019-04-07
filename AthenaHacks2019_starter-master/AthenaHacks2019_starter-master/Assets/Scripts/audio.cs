1a1bdd2b31ed69490c36f30ed1f59d3221f5393e
airada <jasmineirada@gmail.com>
2019-04-07 08:47:03 -0700

    a ton of stuff omg

---
+++ AthenaHacks2019_starter-master/AthenaHacks2019_starter-master/Assets/Scripts/audio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour
+{
+    public float num;
+    public GameObject trigger;
+    public AudioSource audioClip;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider collider)
+    {
+            trigger.SetActive(true);
+            audioClip.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
